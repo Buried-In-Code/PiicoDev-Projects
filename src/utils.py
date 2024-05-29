5418f75f3030ab9ac1ccb68e65e9d5da283e7dd6
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2024-05-29 16:01:16 +1200

    Fix day allocation

--- src/utils.py
+++ src/utils.py
@@ -11,13 +11,13 @@ class Bin:
 
 
 class Day:
-    SUN = 0
-    MON = 1
-    TUE = 2
-    WED = 3
-    THU = 4
-    FRI = 5
-    SAT = 6
+    MON = 0
+    TUE = 1
+    WED = 2
+    THU = 3
+    FRI = 4
+    SAT = 5
+    SUN = 6
 
 
 def get_datetime(offset: int = offset) -> tuple[int, int, int, int, int, int, int, int]:
